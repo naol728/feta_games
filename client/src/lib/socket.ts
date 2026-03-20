659a9bd71cfa5828c69ee6383f1c49918a7dbb6f
naol meseret <naolmeseret22@gmail.com>
2026-03-20 12:04:46 +0300

    done

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io("https://feta-games.onrender.com", {
   transports: ["websocket"],
   withCredentials: true,
 });

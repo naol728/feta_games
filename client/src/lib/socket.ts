ce653be255d2937f2fc021c1423dd8a15a4161ef
naol meseret <naolmeseret22@gmail.com>
2026-03-23 21:02:56 +0300

    done

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io(import.meta.env.VITE_BACKEND_URL!, {
   transports: ["websocket"],
   withCredentials: true,
 });

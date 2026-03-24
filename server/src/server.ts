a73d516bda9a8b863fc5cb5d28e1679a55d95865
naol meseret <naolmeseret22@gmail.com>
2026-03-24 09:40:06 +0300

    done nnnnn

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,8 @@
 import server from "./app";
 import { env } from "./config/env";
 
-server.listen(env.PORT || 3000, () => {
+server.listen(env.PORT, () => {
   console.log(`🚀 Server running on port ${env.PORT}`);
   console.log(`🌐 CORS enabled for: ${env.CLIENT_URL}`);
   console.log(`📡 WebSocket server ready at ws://localhost:${env.PORT}`);
 });
-G

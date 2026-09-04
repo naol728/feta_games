20d4848fc6060ab6098b513f61ae5672279a24a9
naol <naolmeseret22@gmail.com>
2026-09-04 17:40:24 +0300

    done with waggering

--- client/src/pages/player/Profile.tsx
+++ client/src/pages/player/Profile.tsx
@@ -182,7 +182,7 @@ export default function Profile() {
             return;
         }
 
-        if (user?.wallets?.available_balance < amount) {
+        if ((user?.wallets?.available_balance ?? 0) < amount) {
             toast.error("Insufficient balance");
             return;
         }
